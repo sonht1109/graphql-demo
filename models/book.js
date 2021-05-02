022cd9f8744caf27a6fa5e4f7b98977becd31270
Son <sonht1109@gmail.com>
2021-05-02 22:47:59 +0700

    update

--- models/book.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const bookSchema = new mongoose.Schema({
-  name: String,
-  genre: String,
-  authorId: String
-})
-
-module.exports = mongoose.model('Book', bookSchema)
